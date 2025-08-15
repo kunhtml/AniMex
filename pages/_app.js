2323def13c204a421d3ec136e7462c868be589d3
kunhtml <94962460+kunhtml@users.noreply.github.com>
2025-08-16 03:32:14 +0700

    update

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import '../styles/globals.css';
+import Head from 'next/head';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Thư viện sách số — Demo</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
